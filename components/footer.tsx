6861d915ab2c508ef67f22480144df345a41e3fd
Ankur1493 <ankursharma1493@gmail.com>
2024-05-06 20:57:44 +0530

    added footer

---
+++ components/footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div className="text-gray-300 text-[20px] w-screen flex justify-between fixed bottom-0 py-3 px-6 border-t border-white">
+      <h1>Karya is always there to help you</h1>
+      <h1>Create by <a className="text-gray-200" href="https://github.com/ankur1493">Ankur Sharma</a> </h1>
+    </div>
+  )
+}
+
+export default Footer 
